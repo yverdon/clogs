d0571908e720f76c3f4f3b5ebec3a72a412a80ef
monodo <olivier.monod@yverdon-les-bains.ch>
2024-06-05 16:32:01 +0200

    lint

--- clogs/themes/api.py
+++ clogs/themes/api.py
@@ -1,20 +1,19 @@
 from typing import List
-from ninja import NinjaAPI, Schema, ModelSchema
+
+from ninja import NinjaAPI
 from ninja.orm import create_schema
+
 from . import models
 
 api = NinjaAPI()
 
 
 ThemeSchema = create_schema(
-    models.Theme,
-    depth=10,
-    fields=['name', 'functionality', 'layergroupmp']
+    models.Theme, depth=10, fields=["name", "functionality", "layergroupmp"]
 )
 
 
-@api.get("/themes", response=List[ThemeSchema])
+@api.get("/themes", response=list[ThemeSchema])
 def themes(request):
     queryset = models.Theme.objects.all()
     return list(queryset)
-
