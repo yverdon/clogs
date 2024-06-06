efb5ae14c31b090c442b1a7da4962cbd8365a383
monodo <olivier.monod@yverdon-les-bains.ch>
2024-06-06 17:17:48 +0200

    more ninjas

--- clogs/themes/api.py
+++ clogs/themes/api.py
@@ -1,19 +1,84 @@
 from typing import List
 
-from ninja import NinjaAPI
-from ninja.orm import create_schema
+from ninja import ModelSchema, NinjaAPI, Schema
 
 from . import models
 
 api = NinjaAPI()
 
 
-ThemeSchema = create_schema(
-    models.Theme, depth=10, fields=["name", "functionality", "layergroupmp"]
-)
+class FunctionalitySchema(Schema):
+    id: int
+    name: str
+    value: str
+
+
+class MetadataSchema(Schema):
+    id: int
+    name: str
+    value: str
+
+
+class LayerSchema(ModelSchema):
+    class Meta:
+        model = models.Layer
+        fields = ["id", "name"]
+
+
+class OgcserverSchema(ModelSchema):
+    class Meta:
+        model = models.Layer
+        fields = ["id", "name"]
+
+
+class LayerGroupSchema(Schema):
+    id: int
+    name: str
+    layer: list[LayerSchema] = []
+
+
+class ThemeSchema(Schema):
+    id: int
+    name: str
+    icon: str
+    layergroupmp: list[LayerGroupSchema] = []
+    functionality: list[FunctionalitySchema] = []
+    metadata: list[MetadataSchema] = []
+
+
+class GeogirafeSchema(Schema):
+    ogcServer: list[OgcserverSchema] = []
+    themes: list[ThemeSchema] = []
+    backgroud_layers: str = None
+    errors: str = None
+
+
+# https://github.com/fabiocaccamo/django-treenode/discussions/89
+
+
+@api.get("/layers", response=list[LayerSchema])
+def themes(request):
+    queryset = models.Layer.objects.all()
+    return queryset
 
 
 @api.get("/themes", response=list[ThemeSchema])
 def themes(request):
-    queryset = models.Theme.objects.all()
-    return list(queryset)
+    queryset = (
+        models.Theme.objects.prefetch_related("functionality")
+        .prefetch_related("metadata")
+        .prefetch_related("layergroupmp")
+        .prefetch_related("layergroupmp__layer")
+    )
+    return queryset
+
+
+@api.get("/geogirafe", response=list[GeogirafeSchema])
+def themes(request):
+    queryset = (
+        models.Theme.objects.prefetch_related("functionality")
+        .prefetch_related("metadata")
+        .prefetch_related("layergroupmp")
+        .prefetch_related("layergroupmp__layer")
+    )
+    return queryset
